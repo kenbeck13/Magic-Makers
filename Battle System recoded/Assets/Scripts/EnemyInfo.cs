af7bba7ccc47af60232eddafc7d22931d01b81bf
kenbeck13 <kenbeck13@gmail.com>
2018-11-29 15:48:46 -0500

    GameInfo and EnemyInfo

---
+++ Battle System recoded/Assets/Scripts/EnemyInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyInfo : MonoBehaviour {
+
+    public string[] enemy_name;
+    public int[] enemy_attack;
+    public int[] enemy_max_health;
+    public int[] enemy_speed;
+    public string[] enemy_attack_name_1;
+    public string[] enemy_attack_name_2;
+
+	// Use this for initialization
+	void Start () {
+        DontDestroyOnLoad(gameObject);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+	}
+}
---
+++ Battle System recoded/Assets/Scripts/GameInfo.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameInfo : MonoBehaviour {
+
+    int[] enemiesToBattle;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        StartBattle(enemiesToBattle);
+	}
+
+    void StartBattle(int[] enemies) {
+        
+    }
+}
