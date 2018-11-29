1dad1c13f208c07d11d78d5e14ea38822b3a3077
kenbeck13 <kenbeck13@gmail.com>
2018-11-29 15:14:34 -0500

    Player Info divided, enemy name text file

--- Battle System recoded/Assets/Scripts/PlayerInfo.cs
+++ Battle System recoded/Assets/Scripts/PlayerInfo.cs
@@ -9,7 +9,7 @@ public class PlayerInfo : MonoBehaviour {
     public int player_max_health;
     public int player_current_health;
     public int player_max_magic;
-    public int player_magic;
+    public int player_current_magic;
     public int player_defense;
     public int player_speed;
     //spell info
@@ -32,6 +32,8 @@ public class PlayerInfo : MonoBehaviour {
     public int player_air_level;
     public int player_light_level;
     public int player_dark_level;
+    //level up requirements
+    public int[] level_up_requirements;
     //element skill tree unlocks
     public bool[] player_fire_unlocks;
     public bool[] player_water_unlocks;
@@ -42,11 +44,45 @@ public class PlayerInfo : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
+        player_current_health = player_max_health;
+        player_current_magic = player_max_magic;
+        DontDestroyOnLoad(gameObject);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        //if(Input.GetMouseButtonDown(0)){
+        //    player_fire_exp++;
+        //    player_water_exp++;
+        //    player_earth_exp++;
+        //    player_air_exp++;
+        //    player_light_exp++;
+        //    player_dark_exp++;
+        //}
+
+        if(player_fire_exp >= level_up_requirements[player_fire_level])
+        {
+            player_fire_level++;
+        }
+        if (player_water_exp >= level_up_requirements[player_water_level])
+        {
+            player_water_level++;
+        }
+        if (player_earth_exp >= level_up_requirements[player_earth_level])
+        {
+            player_earth_level++;
+        }
+        if (player_air_exp >= level_up_requirements[player_air_level])
+        {
+            player_air_level++;
+        }
+        if (player_light_exp >= level_up_requirements[player_light_level])
+        {
+            player_light_level++;
+        }
+        if (player_dark_exp >= level_up_requirements[player_dark_level])
+        {
+            player_dark_level++;
+        }
 	}
 }
